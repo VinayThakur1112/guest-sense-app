9741685056e1ad64032da7cbeec477ceefce1a37
Vinay Thakur <vinayrsinghthakur@gmail.com>
2025-10-06 18:42:53 +0530

    tf modified

--- infra/terraform/modules/resource_group/main.tf
+++ infra/terraform/modules/resource_group/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 }
 
 # Create an Azure Resource Group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
