44319bca0e7c2079731109b24d8f4d8bed11c953
Vinay Thakur <vinayrsinghthakur@gmail.com>
2025-10-08 19:20:59 +0530

    aks acr added

--- infra/terraform/modules/container_registry/main.tf
+++ infra/terraform/modules/container_registry/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.name
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = true
